209efcaa1a65d6090416b0d84c3beb8f300cd7fa
Aviatheist <avirameshparajuli@gmail.com>
2026-04-07 12:42:12 +0545

    migration modified

--- Blogs/models.py
+++ Blogs/models.py
@@ -7,9 +7,7 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-STATUS_CHOICES = (
-    ('draft', 'Draft'),
-    ('published', 'Published'),
+STATUS_CHOICES = ((0, 'Draft'), (1, 'Published'))   
 )   
 class Blog(models.Model):
     title = models.CharField(max_length=200)
@@ -20,7 +18,7 @@ class Blog(models.Model):
     featured_image = models.ImageField(upload_to='uploads/%y/%m/%d/', null=True, blank=True)
     short_description = models.CharField(max_length=500, null=True, blank=True)
     blog_body = models.TextField(max_length=5000, null=True, blank=True)
-    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=draft)
     featured_post = models.BooleanField(default=False, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
