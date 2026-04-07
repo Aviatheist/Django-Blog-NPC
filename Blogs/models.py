e10feb334fdce595ddda130c4ae57c967e29c9af
Aviatheist <avirameshparajuli@gmail.com>
2026-04-07 12:00:15 +0545

    Blog model added

--- Blogs/models.py
+++ Blogs/models.py
@@ -7,3 +7,23 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+STATUS_CHOICES = (
+    ('draft', 'Draft'),
+    ('published', 'Published'),
+)   
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    featured_image = models.ImageField(upload_to='uploads/%y/%m/%d/', null=True, blank=True)
+    short_description = models.CharField(max_length=500, null=True, blank=True)
+    blog_body = models.TextField(max_length=5000, null=True, blank=True)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
+    featured_post = models.BooleanField(default=False, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
