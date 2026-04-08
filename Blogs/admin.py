a2dd7a08a6f9966f6d93328266c16649dd09163c
Aviatheist <avirameshparajuli@gmail.com>
2026-04-08 12:06:37 +0545

    Urls modified

--- Blogs/admin.py
+++ Blogs/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Category
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'author', 'status', 'featured_post', 'created_at', 'category')
+    search_fields = ('title', 'category__name', 'author__username', 'status')
+    list_editable = ('status', 'featured_post', 'author', 'category')
 from .models import Blog
 admin.site.register(Category)
-admin.site.register(Blog)
+admin.site.register(Blog, BlogAdmin)
+
 
--- Blogs/models.py
+++ Blogs/models.py
@@ -8,7 +8,6 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 STATUS_CHOICES = ((0, 'Draft'), (1, 'Published'))   
-)   
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
@@ -18,7 +17,7 @@ class Blog(models.Model):
     featured_image = models.ImageField(upload_to='uploads/%y/%m/%d/', null=True, blank=True)
     short_description = models.CharField(max_length=500, null=True, blank=True)
     blog_body = models.TextField(max_length=5000, null=True, blank=True)
-    status = models.IntegerField(choices=STATUS_CHOICES, default=draft)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
     featured_post = models.BooleanField(default=False, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
